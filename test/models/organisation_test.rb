a977d5659bdbb77c08b74c7f7614eeb9940503d1
BK <benkoshy@hotmail.com>
2019-12-30 15:20:32 +1100

    Add: sample organisation test

--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -1,9 +1,8 @@
-
 require 'test_helper'
-
+require 'sequel_test_case'
 
 class OrganisationTest < SequelTestCase
   def test_organisation_name
-    assert true
+    assert_equal(true, false)
   end
 end
