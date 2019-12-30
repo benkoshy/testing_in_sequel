4a3620a2700d54a1bf732dda82f68bbe80aa46f0
BK <benkoshy@hotmail.com>
2019-12-30 16:35:25 +1100

    Add: define DB in the test

--- test/sequel_test_case.rb
+++ test/sequel_test_case.rb
@@ -1,3 +1,7 @@
+require 'sequel'
+
+DB = Sequel.postgres # memory database, requires sqlite3
+
 class SequelTestCase < Minitest::Test
   def run(*args, &block)
     DB.transaction(:rollback=>:always, :auto_savepoint=>true){super}
